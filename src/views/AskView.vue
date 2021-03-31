d83867b7e79aa337a22f6689b70d12eef0c1a7b9
itsjustsora <itsyoursora@gmail.com>
2021-03-31 17:09:31 +0900

    API 구현

---
+++ src/views/AskView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div v-for="user in users" v-bind:key="user.id">{{ user.title }}</div>
+  </div>
+</template>
+
+<script>
+  import { fetchAskList } from '../api/index.js';
+
+  export default {
+    data() {
+      return {
+        users: []
+      }
+    },
+    created() {
+      var vm = this;
+      fetchAskList()
+        .then(response => vm.users = response.data)
+        .catch(error => console.log(error))
+    },
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/views/JobsView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div v-for="user in users" v-bind:key="user.id">{{ user.title }}</div>
+  </div>
+</template>
+
+<script>
+import { fetchJobsList } from '../api/index.js';
+
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  created() {
+    var vm = this;
+    fetchJobsList()
+      .then(response => vm.users = response.data)
+      .catch(error => console.log(error))
+  },
+}
+</script>
+
+<style>
+
+</style>
