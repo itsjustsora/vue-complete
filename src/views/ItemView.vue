11814644ca011c973e40b1954b5a2710a840c7f9
keemsora <80027033+itsjustsora@users.noreply.github.com>
2021-04-05 17:50:46 +0900

    사용자 프로필 컴포넌트 공통화

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,42 +1,38 @@
 <template>
   <div>
     <section> 
-      <!-- 질문 상세 정보 -->
-      <div class="user-container">
-        <div>
-          <i class="fas fa-user"></i> 
-        </div>
-        <div class="user-description">
-          <router-link :to="`/user/${fetchedItem.user}`">
+      <!-- 사용자 정보 -->
+      <user-profile :info="fetchedItem">
+        <router-link slot="username" :to="`/user/${fetchedItem.user}`">
             {{ fetchedItem.user }}
           </router-link>
-          <div class="time">
-            {{ fetchedItem.time_ago  }}
-          </div>
-        </div>
-      </div>
+        <!-- <div slot="username">{{ fetchedItem.user }}</div> -->
+        <template slot="time">{{ 'Posted ' + fetchedItem.time_ago }}</template>
+      </user-profile>
+    </section>
+    <section>
       <h2>{{ fetchedItem.title }}</h2>
     </section>
     <section>
       <!-- 질문 댓글 -->
+      <div v-html="fetchedItem.content"></div>
     </section>
-    <!-- <p>{{ fetchedItem.title }}</p>
-    <div>{{ fetchedItem.content }}</div> -->
-    <div v-html="fetchedItem.content">
-      <!-- {{ fetchedItem.content }} -->
-    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import UserProfile from '../components/UserProfile.vue';
+
 export default {
+  components: {
+    UserProfile,
+  },
   computed: {
       ...mapGetters(['fetchedItem']),
   },
   created() {
     const itemId = this.$route.params.id;
-    // console.log(itemId);
     this.$store.dispatch('FETCH_ITEM', itemId);
   }
 }
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,22 +1,27 @@
 <template>
   <div>
-    <p>name : {{ userInfo.id }}</p>
-    <p>karma : {{ userInfo.karma }}</p>
-    <p>created : {{ userInfo.created }}</p>
+    <user-profile :info="userInfo">
+      <div slot="username">{{ userInfo.id }}</div>
+      <span slot="time">{{ 'Joined ' + userInfo.created }}, </span>
+      <span slot="karma">{{ userInfo.karma }}</span>
+    </user-profile>
   </div>
 </template>
 
 <script>
+import UserProfile from '../components/UserProfile.vue';
+
 export default {
+  components: {
+    UserProfile,
+  },
   computed: {
     userInfo() {
       return this.$store.state.user;
     }
   },
   created() {
-    // console.log(this.$route.params.id);
     const userName = this.$route.params.id;
-    // axios.get(`https://api.hnpwa.com/v0/user/${ userName}.json`) 
     this.$store.dispatch('FETCH_USER', userName);
   }
 
